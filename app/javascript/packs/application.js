68982581c7d2eb0d06f41d88982d8b6609c26785
Mathieu Fontaine <mathieu.antoine.fontaine@gmail.com>
2019-12-16 16:03:27 +0000

    stuff

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,17 @@
+
 import "bootstrap";
+
+//= require jquery
+//= require jquery_ujs
+//= require jquery.turbolinks
+//= require bootstrap-sprockets
+//= require turbolinks
+//= require_tree .
+
+
+import {sortableTable} from '../pluggins/sortable_table.js';
+
+sortableTable();
+
+
+
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -15,3 +15,11 @@ environment.plugins.prepend('Provide',
 )
 
 module.exports = environment
+
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
+  })
+)
