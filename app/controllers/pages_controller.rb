60ccbffbca7840e273b2b37cddc2c1cc6bb4ba99
Mathieu Fontaine <mathieu.antoine.fontaine@gmail.com>
2019-12-20 17:57:26 +0100

    home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,17 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :show]
 
   def home
+
+  	if params[:query].present?
+  	  sql_query = " \
+  	    pokemons.name ILIKE :query \
+  	    OR pokemons.location ILIKE :query \
+  	    OR types.name ILIKE :query \
+  	    OR moves.name ILIKE :query \
+  	  "
+  	  @pokemons = policy_scope(Pokemon).joins(:type).where(sql_query, query: "%#{params[:query]}%")
+  	else
+  	@pokemons = policy_scope(Pokemon)
+  	end
   end
 end
